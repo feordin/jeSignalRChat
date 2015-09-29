d5d2ae64905661351697093a66f18dac51a75a9c
feordin <jared.erwin@gmail.com>
2015-09-29 15:30:35 -0700

    First push of full code

---
+++ jeSignalRChat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jeSignalRChat.MvcApplication" Language="C#" %>
---
+++ jeSignalRChat/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(jeSignalRChat.Startup))]
+namespace jeSignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            string connectionString = "Endpoint=sb://jesignalr.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=I8UUxgBVJhmByn9vO3XZQCz4TW0KeUBJ6p4uNwfJ2h0=";
+            GlobalHost.DependencyResolver.UseServiceBus(connectionString, "Chat");
+
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
